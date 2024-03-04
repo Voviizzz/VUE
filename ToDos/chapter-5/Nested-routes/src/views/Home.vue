bc1c2eefe536c44f5a8020a9207bc27d27dd0e6e
Vladimir Zaytsev <ivovochka48@gmial.com>
2024-03-04 16:09:54 +0300

    spa

---
+++ ToDos/chapter-5/Nested-routes/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+    
+</script>
+
+<template>
+    <h1>Chapter 5: Nested routes example</h1>
+    <p>This example shows how to define and view nested routes with default views.</p>
+    <RouterLink :to="{name:'directory'}">Open the directory</RouterLink>
+</template>
+
+<style scoped>
+    
+</style>

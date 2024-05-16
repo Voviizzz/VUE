8c5246ab3278086701dd7220b786c3b7d729bf4b
Vladimir Zaytsev <ivovochka48@gmial.com>
2024-05-16 16:52:47 +0300

    sideBar example

---
+++ SideBar/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="about-page">
+      <h1>О нас</h1>
+      <p>Тут вы можете прочитать о нас</p>
+    </main>
+  </template>
+  
---
+++ SideBar/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="home-page">
+    <h1>Главная</h1>
+    <p>Добро пожаловать на главную</p>
+  </main>
+</template>
